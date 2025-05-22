3495afbb235407687696d3a6b57ed69d9aad1550
Joe McDonald <joemcd0224@gmail.com>
2025-05-22 08:04:41 -0700

    Added MSTest unit tests and GitHub workflow

---
+++ Rift.Tests/UnitTest.cs
@@ -0,0 +1,17 @@
+﻿namespace Rift.Tests;
+
+[TestClass]
+public sealed class Test1
+{
+    // Sample unit test
+    [TestMethod]
+    public void AddingTwoNumbers_ReturnsCorrectSum()
+    {
+        int a = 2;
+        int b = 3;
+        int sum = a + b;
+
+        Assert.AreEqual(5, sum);
+    }
+
+}
